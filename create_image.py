f69a5c5770708c465990925d946abf8c9b018996
murilobarbosa <murilobarbosa1202@gmail.com>
2023-02-15 12:48:24 +0000

    add comments and delete unnecessary packages/variables

--- create_image.py
+++ create_image.py
@@ -1,5 +1,4 @@
 import os
-import time
 import cv2
 
 # get all file with .mp4 prefix
@@ -7,27 +6,29 @@ folder = "/Users/murilobarbosa/Desktop/wonk/videos"
 results = []
 results += [each for each in os.listdir(f"{folder}") if each.endswith(".mp4")]
 
+# for loop that capture and save each 3s of video
 for i in results:
-    start = time.time()
+    # read video
     cap = cv2.VideoCapture(f'{folder}/{i}')
     count = 0
     success = True
     fps = int(cap.get(cv2.CAP_PROP_FPS))
 
+    # define the interval in seconds
     if fps > 0:
         frame_interval = 3 * fps
     else:
         frame_interval = 30  # default value
 
+    # create images from video
     while success:
         success, image = cap.read()
         if not success:
             break
         if count % frame_interval == 0:
             cv2.imwrite(f'/Users/murilobarbosa/Desktop/wonk/images/{i[:-11]}_{count}.jpg', image)
-            # cv2.imshow("Frame", image)
         count += 1
 
+    # delete all videos in this folder
     os.remove(f'{folder}/{i}')
-
     cap.release()
